0c6117e6632e7ac10ab6b919c2b95ffc9750838b
KALORSOR <alorsorgodsway@gmail.com>
2024-03-08 07:28:49 -0800

    models

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-'''comment'''
-from models.file_storage import FileStorage
+"""Create a unique FileStorage instance for your application"""
+from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
